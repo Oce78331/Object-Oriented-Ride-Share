05287f8149d3ab259c983e00d9c2479b23d4a495
Oce78331 <123347260+Oce78331@users.noreply.github.com>
2025-06-01 22:25:23 +0530

    Add files via upload

---
+++ lib/trip.rb
@@ -0,0 +1,71 @@
+require 'csv'
+
+require_relative 'csv_record'
+
+module RideShare
+  class Trip < CsvRecord
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+
+    def initialize(
+          id:,
+          passenger: nil,
+          passenger_id: nil,
+          start_time:,
+          end_time:,
+          cost: nil,
+          rating:
+        )
+      super(id)
+
+      if passenger
+        @passenger = passenger
+        @passenger_id = passenger.id
+
+      elsif passenger_id
+        @passenger_id = passenger_id
+
+      else
+        raise ArgumentError, 'Passenger or passenger_id is required'
+      end
+
+      @start_time = start_time
+      @end_time = end_time
+      @cost = cost
+      @rating = rating
+
+      if @rating > 5 || @rating < 1
+        raise ArgumentError.new("Invalid rating #{@rating}")
+      end
+    end
+
+    def inspect
+      # Prevent infinite loop when puts-ing a Trip
+      # trip contains a passenger contains a trip contains a passenger...
+      "#<#{self.class.name}:0x#{self.object_id.to_s(16)} " +
+        "id=#{id.inspect} " +
+        "passenger_id=#{passenger&.id.inspect} " +
+        "start_time=#{start_time} " +
+        "end_time=#{end_time} " +
+        "cost=#{cost} " +
+        "rating=#{rating}>"
+    end
+
+    def connect(passenger)
+      @passenger = passenger
+      passenger.add_trip(self)
+    end
+
+    private
+
+    def self.from_csv(record)
+      return self.new(
+               id: record[:id],
+               passenger_id: record[:passenger_id],
+               start_time: record[:start_time],
+               end_time: record[:end_time],
+               cost: record[:cost],
+               rating: record[:rating]
+             )
+    end
+  end
+end
